2cc32da3147f5171603a420ca9309d2b5fed3193
Bishobos <mps.kajkage@gmail.com>
2026-04-08 11:18:55 +0200

    initial commit

---
+++ src/test/java/wishlistgr6/wishlist/WishlistApplicationTests.java
@@ -0,0 +1,13 @@
+package wishlistgr6.wishlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishlistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
