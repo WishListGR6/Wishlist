489e6c3be879ddab734b6e8f2b21bd7705e07743
Bishobos <mps.kajkage@gmail.com>
2026-04-14 10:13:25 +0200

    skeleton of login

--- src/main/java/wishlistgr6/wishlist/controller/WishlistController.java
+++ src/main/java/wishlistgr6/wishlist/controller/WishlistController.java
@@ -1,13 +1,57 @@
 package wishlistgr6.wishlist.controller;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import wishlistgr6.wishlist.model.Wishlist;
 import wishlistgr6.wishlist.service.WishlistService;
+
+import java.util.Random;
+import java.util.regex.Pattern;
 
 @Controller
 @RequestMapping("/")
 public class WishlistController {
     private final WishlistService service;
+    private Wishlist wishlist;
+    private boolean isOwner;
     public WishlistController(WishlistService service) {this.service = service;}
+
+    private boolean isLoggedIn(HttpSession session){
+        return session.getAttribute("isOwner") !=null;
+    }
+
+    @GetMapping("/login/{listID}")
+    public String showLogin(@PathVariable String listID, @RequestParam String accessToken, Model model, HttpSession session){
+        //check if acessToken is good
+        //pre-authenticated login
+        if(service.getAccessTokens(listID).contains(accessToken) && accessToken != null){
+            session.setAttribute("wishlist", service.getWishlist(listID));
+            session.setAttribute("isOwner", false);
+            return "redirect:/wishlist";
+        }
+
+        model.addAttribute("listID", listID);
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute String password, Model model, HttpSession session) {
+        if (service.checkOwnerPassword(listID, password)){
+            session.setAttribute("isOwner", true);
+            session.setAttribute("wishlist", service.getWishlist(model.getAttribute("listID")));
+            return "redirect:/wishlist";
+        }
+
+        if (service.checkGuestPassword(ListID, password)){
+            session.setAttribute("isOwner", false);
+            session.setAttribute("wishlist", service.getWishlist(model.getAttribute("listID")));
+            return "redirect:/wishlist";
+        }
+
+        model.addAttribute("invalidPassword", true);
+        return "redirect:/login/" + model.getAttribute("listID");
+    }
 
 }
--- src/main/java/wishlistgr6/wishlist/repository/WishlistRepository.java
+++ src/main/java/wishlistgr6/wishlist/repository/WishlistRepository.java
@@ -3,10 +3,27 @@ package wishlistgr6.wishlist.repository;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.util.Random;
+
 @Repository
 public class WishlistRepository {
     private final JdbcTemplate jdbcTemplate;
 
     public WishlistRepository(JdbcTemplate jdbcTemplate) {this.jdbcTemplate = jdbcTemplate;}
+
+    private String generateAccessToken(){
+
+        int leftLimit = 48;
+        int rightLimit = 122;
+        int targetStringLength = 8;
+
+        Random random = new Random();
+
+        return random.ints(leftLimit, rightLimit + 1)
+                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
+                .limit(targetStringLength)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+    }
 
 }
